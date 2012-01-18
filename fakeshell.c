f9f7bd10ff034533598f25985ea25a3ef2f0ff5f
Meng ZHuo <mengzhuo1203@gmail.com>
2012-01-19 00:44:28 +0800

    add time

--- fakeshell.c
+++ fakeshell.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define MAX_HOSTNAME	255
 
@@ -26,13 +27,19 @@ main(int argc)
 	printf("In case of any urgency, do not hesitate to contact us at %s.\n", ADMINS);
 
 	char c;
+	time_t t;
 	while(c != EOF)
 	{
 		c = getchar();
 		if(c == '\n')
 		{
 			printf("What do you think I am, a shell?\n");
+			time(&t);
+            printf("now date and time: %s", ctime(&t));
+			///fflush_all();
 			fflush(stdout);
+
+
 		}
 	}
 	return (0);
